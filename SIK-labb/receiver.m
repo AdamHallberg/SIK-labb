f715ab0688334a82b183005655ae0c0e81cf06b7
Adam <ah.adamhallberg@gmail.com>
2024-04-08 15:51:30 +0200

    Labb 1 prep

--- SIK-labb/receiver.m
+++ SIK-labb/receiver.m
@@ -1,3 +1,43 @@
 function [zI,zQ,A,tau] = receiver(y)
+%%  Constants
+L  = length(y);                 % Antal punkter i indatan
+fs = 400e3;                     % Samplingsfrekvens
+T  = 1/fs;                      % Periodtid 
+t  = T*[0:L-1];                 % Tidsintervall det samplas över
+%%  De-modulate 
+fc=150e3;                       % Bärfrekvensen
+carrierI=2*cos(2*pi*fc*t).';    % I carrier
+carrierQ=-2*sin(2*pi*fc*t).';   % Q carrier
 
+yI=y.*carrierI;
+yQ=y.*carrierQ;
+
+%%  Low-pass-filter
+N  = 100;                       % Filtrets gradtal
+f0 = 1/20;                      % Normerad frekvens
+
+% Create filter
+[b,a]=fir1(N,f0);               % Designa filter
+yI2 = filter(b,a,yI);           % Filtrera xI2
+yQ2 = filter(b,a,yQ);           % Filtrera xQ2
+
+yI3= filter(b,a,[yI2;zeros(N/2,1)]);
+yI3= yI3(N/2+1:max(size(yI2))); % Remove first N/2 points since filter adds delay
+yQ3= filter(b,a,[yQ2;zeros(N/2,1)]);
+yQ3= yQ3(N/2+1:max(size(yQ2)));
+
+%% Down-sample
+% Constants
+M   = 20;                       % Nedsamplingsfaktor
+L  = L*M;                       % Nytt antal punkter
+T2  = T/M;                      % Ny periodtid
+
+
+% Uppsample signals
+zI  =downsample(yI2,M);         % zI nedsamplad yI2
+zQ  =downsample(yQ2,M);         % zQ nedsamplad yQ2
+
+%%  Finnish
+A   = 1;
+tau = 1;
 end
--- SIK-labb/sender.m
+++ SIK-labb/sender.m
@@ -1,10 +1,50 @@
-function [zI,zQ,A,tau] = sender(y)
+function [x] = sender1(xI, xQ)
+%% Define constants
+L  = length(xI);                % Antal punkter i indatan
+fs = 20e3;                      % Samplingsfrekvens
+T  = 1/fs;                      % Periodtid 
 
-% Sender function
+%% Uppsample
+% Constants
+M   = 20;                       % Uppsamplingsfaktor
+L2  = L*M;                      % Nytt antal punkter
+T2  = T/M;                      % Ny periodtid
+t2=T2*[0:L2-1];                 % Nytt tidsintervall
 
-%% Inläsning
-[xI, fsI] = audioread("xI.wav");
-[xQ, fsQ] = audioread("xQ.wav");
 
+% Uppsample signals
+xI2=upsample(xI,M);             % x2I Uppsamplad xI
+xQ2=upsample(xQ,M);             % x2Q Uppsamplad xQ
 
-end;
+%% Filtrering
+% Filterconstants
+N  = 100;                       % Filtrets gradtal
+f0 = 1/M;                       % Normerad frekvens
+
+% Create filter
+[b,a]=fir1(N,f0);               % Designa filter
+                                % The fir1(N, f0) function creates a lowpass-filter and gives its
+                                % parameters in vector b. N gives the order of the filter. The cutoff
+                                % frequency is normalized, meaning it is specified in terms of the 
+                                % Nyquist frequency (half the sampling frequency).
+
+% Filter signals                
+yI = filter(b,a,xI2);           % Filtrera xI2
+yQ = filter(b,a,xQ2);           % Filtrera xQ2
+
+yI2= filter(b,a,[xI2;zeros(N/2,1)]);
+yI2= yI2(N/2+1:max(size(yI2))); % Remove first N/2 points since filter adds delay
+yQ2= filter(b,a,[xQ2;zeros(N/2,1)]);
+yQ2= yQ2(N/2+1:max(size(yQ2)));
+
+%% Modulera
+fc=150e3;                       % Bärfrekvensen
+carrierI=cos(2*pi*fc*t2).';     % I carrier
+carrierQ=-sin(2*pi*fc*t2).';    % Q carrier
+
+zI=yI2.*carrierI;
+zQ=yQ2.*carrierQ;
+
+%% Done
+x = zI + zQ;                    %  I/Q modulation done
+end
