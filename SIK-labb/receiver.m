2a6f76928f7c362e7af7625733576a513a164050
Adam <ah.adamhallberg@gmail.com>
2024-04-12 09:22:17 +0200

    lelz

--- SIK-labb/receiver.m
+++ SIK-labb/receiver.m
@@ -5,23 +5,49 @@ fs = 400e3;                     % Samplingsfrekvens
 T  = 1/fs;                      % Periodtid
 T0 = 1/20000;
 %%  Determine A and tau
+%  Create known chirp
 Length = 20*100000;
 tc = T*[0:Length-1];
 
 known_chirp = chirp(tc,140e3,Length,160e3)';  % samma som i sender
 
-known_chirp = [known_chirp;zeros(length(y)-length(known_chirp),1)];
+% chirp for leading zeros
+known_chirp1 = [known_chirp;zeros(length(y)-length(known_chirp),1)];  
 
-[~, i0] = max(xcorr(known_chirp));            % Gives index for peak in known chirp 
+%  Get index for correlation for leading zeros
+[~, i0] = max(abs(xcorr(known_chirp1)));            % Gives index for peak in known chirp 
 
-[~, i1] = max(xcorr(y, known_chirp));            % Gives index for peak in known chirp
+[~, i1] = max(abs(xcorr(y, known_chirp1)));            % Gives index for peak in known chirp
 
 added_samples = abs(i1-i0);
+disp("  >>  Added zeros at start: " + added_samples)
 tau = T*added_samples;
-A   = norm(y(added_samples+1:added_samples+Length))/norm(known_chirp);
+
+%%  Bestäm A
+val = xcorr(y, known_chirp1);
+sgn = sign(val(i1));
+A   = norm(y(added_samples+1:added_samples+Length))/norm(known_chirp1);
+A = A*sgn;
 
 %%  Adjust in-signal
 y = (1/A)*y(added_samples + Length + 1:end);
+
+%%  Find trailing zeros
+% chirp for ending zeros
+known_chirp2 = [zeros(length(y)-length(known_chirp),1);known_chirp];  
+
+%  Get index for correlation for leading zeros
+[~, i0] = max(abs(xcorr(known_chirp2)));            % Gives index for peak in known chirp 
+
+[~, i1] = max(abs(xcorr(y, known_chirp2)));            % Gives index for peak in known chirp
+
+added_samples = abs(i1-i0);
+disp("  >>  Added zeros at end:   " + added_samples)
+
+%%  Adjust in-signal
+%sze = size(y)                                % When crash = 9978
+%result = length(y) -added_samples + Length;  % when crash = 2001711
+y = y(1:end-(added_samples + Length));        % when crash = []
 
 %%  De-modulate 
 t  = T*[0:length(y)-1];                 % Tidsintervall det samplas över
@@ -50,12 +76,11 @@ yQ2 = yQ2(N/2+1:end);
 %% Down-sample
 % Constants
 M   = 20;                       % Nedsamplingsfaktor
-L   = L/M;                      % Nytt antal punkter
+L   = length(y)/M;                      % Nytt antal punkter
 T2  = T/M;                      % Ny periodtid
 
 
 % Uppsample signals
 zI  = downsample(yI2,M);        % zI nedsamplad yI2
 zQ  = downsample(yQ2,M);        % zQ nedsamplad yQ2
-
 end
--- SIK-labb/sender.m
+++ SIK-labb/sender.m
@@ -1,61 +1,63 @@
-function [zI,zQ,A,tau] = receiver(y)
-%%  Constants
-L  = length(y);                 % Antal punkter i indatan
-fs = 400e3;                     % Samplingsfrekvens
-T  = 1/fs;                      % Periodtid
-T0 = 1/20000;
-%%  Determine A and tau
-Length = 20*100000;
-tc = T*[0:Length-1];
+function [x] = sender(xI, xQ)
+%% Define constants
+L  = length(xI);                % Antal punkter i indatan
+fs = 20e3;                      % Samplingsfrekvens
+T  = 1/fs;                      % Periodtid 
 
-known_chirp = chirp(tc,140e3,Length,160e3)';  % samma som i sender
-
-known_chirp = [known_chirp;zeros(length(y)-length(known_chirp),1)];
-
-[~, i0] = max(xcorr(known_chirp));            % Gives index for peak in known chirp 
-
-[~, i1] = max(xcorr(y, known_chirp));            % Gives index for peak in known chirp
-
-added_samples = abs(i1-i0);
-tau = T*added_samples;
-A   = norm(y(added_samples+1:added_samples+Length))/norm(known_chirp);
-
-%%  Adjust in-signal
-y = (1/A)*y(added_samples + Length + 1:end);
-
-%%  De-modulate 
-t  = T*[0:length(y)-1];                 % Tidsintervall det samplas över
-
-fc = 150e3;                       % Bärfrekvensen
-carrierI = 2*cos(2*pi*fc*t).';    % I carrier
-carrierQ = -2*sin(2*pi*fc*t).';   % Q carrier
-
-yI = y.*carrierI;
-yQ = y.*carrierQ;
-
-%%  Low-pass-filter
-N  = 100;                       % Filtrets gradtal
-f0 = 1/20;                      % Normerad frekvens
-
-% Create filter
-[b,a]=fir1(N,f0);               % Designa filter
-
-% Handle time delay
-yI2 = filter(b,a,[yI;zeros(N/2,1)]);
-yQ2 = filter(b,a,[yQ;zeros(N/2,1)]);
-
-yI2 = yI2(N/2+1:end);           % Remove first N/2 points since filter adds delay
-yQ2 = yQ2(N/2+1:end);
-
-%% Down-sample
+%% Uppsample
 % Constants
-M   = 20;                       % Nedsamplingsfaktor
-L   = L/M;                      % Nytt antal punkter
+M   = 20;                       % Uppsamplingsfaktor
+L2  = L*M;                      % Nytt antal punkter
 T2  = T/M;                      % Ny periodtid
+t2=T2*[0:L2-1];                 % Nytt tidsintervall
 
 
 % Uppsample signals
-zI  = downsample(yI2,M);        % zI nedsamplad yI2
-zQ  = downsample(yQ2,M);        % zQ nedsamplad yQ2
+xI2=upsample(xI,M);             % x2I Uppsamplad xI
+xQ2=upsample(xQ,M);             % x2Q Uppsamplad xQ
+
+%% Filtrering
+% Filterconstants
+N  = 100;                       % Filtrets gradtal
+f0 = 1/M;                       % Normerad frekvens
+
+% Create filter
+[b,a]=fir1(N,f0);               % Designa filter
+                                % The fir1(N, f0) function creates a lowpass-filter and gives its
+                                % parameters in vector b. N gives the order of the filter. The cutoff
+                                % frequency is normalized, meaning it is specified in terms of the 
+                                % Nyquist frequency (half the sampling frequency).
+
+% Filter signals                
+%yI = filter(b,a,xI2);           % Filtrera xI2
+%yQ = filter(b,a,xQ2);           % Filtrera xQ2
+
+yI2= filter(b,a,[xI2;zeros(N/2,1)]);
+yI2= yI2(N/2+1:max(size(yI2))); % Remove first N/2 points since filter adds delay
+yQ2= filter(b,a,[xQ2;zeros(N/2,1)]);
+yQ2= yQ2(N/2+1:max(size(yQ2)));
+
+%% Modulera
+fc=150e3;                       % Bärfrekvensen
+carrierI=cos(2*pi*fc*t2).';     % I carrier
+carrierQ=-sin(2*pi*fc*t2).';    % Q carrier
+
+zI=M*yI2.*carrierI;
+zQ=M*yQ2.*carrierQ;
+
+x = zI + zQ;                    %  I/Q modulation done
+%%  Create chirp
+known_chirp = chirp(t2,140e3,L2,160e3)';
+%%  Add known chirp to signal
+x = [known_chirp; x; known_chirp];
+%%  Bandpass filter to stay in desired bandwidth
+% filter signal
+%[b,a]=fir1(N,[140, 160]/(400));               % Designa filter
+
+%x  = filter(b,a,[x;zeros(N/2,1)]);
+%x  = 400*x(N/2+1:max(size(x))); % Remove first N/2 points since filter adds delay
+
+%size(x)
+
 
 end
